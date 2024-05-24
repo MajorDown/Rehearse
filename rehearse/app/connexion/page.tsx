2822423cefb4da95d464b640c59e42b78d0dfd93
MajorDown <romain.fouillaron@gmx.fr>
2024-05-24 23:01:13 +0200

    page de connexion

--- rehearse/app/connexion/page.tsx
+++ rehearse/app/connexion/page.tsx
@@ -1,10 +1,46 @@
+'use client'
+import { useRef } from "react";
 import PageSection from "@/components/PageSection";
 
 const Records = () => {
-    
+    const emailRef = useRef<HTMLInputElement>(null);
+    const passwordRef = useRef<HTMLInputElement>(null);
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log("submit");
+    };  
+
     return (
-        <PageSection title="Connexion" id="RecordsSection">
-            <p>page de connexion</p>
+        <PageSection title="Connectez-vous" id="connexionSection">
+            <form onSubmit={(event) => handleSubmit(event)}>
+                <div className={"inputWrapper"}>
+                    <label htmlFor="email">Votre Email :</label>
+                    <input 
+                        type="email" 
+                        name="email" 
+                        id="email" 
+                        placeholder="Joeladechirure@jld.fr"
+                        ref={emailRef}
+                        required
+                    />
+                </div>
+                <div className={"inputWrapper"}>
+                    <label htmlFor="password">Votre Mot de passe :</label>
+                    <input 
+                        type="password" 
+                        name="password" 
+                        id="password" 
+                        placeholder="(un autre truc que votre date de naissance)"
+                        ref={passwordRef}
+                        required
+                    />
+                </div>
+                <button type="submit" className={"actionBtn"}>Se connecter</button>
+                <div className={"separator"}>.</div>
+                <p>Vous n'avez pas encore de compte ?</p>
+                <button type="button" className={"actionBtn"}>Inscription</button>
+            </form>
         </PageSection>
     )
 }
