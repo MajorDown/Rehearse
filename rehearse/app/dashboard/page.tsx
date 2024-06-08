c73a3468b903273a70317952d3a41e7211f3189e
MajorDown <romain.fouillaron@gmx.fr>
2024-06-08 23:59:08 +0200

    tests de routage

---
+++ rehearse/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import PageSection from "@/components/PageSection";
+
+
+const Connexion = () => {
+    return (
+        <PageSection title="Dashboard" id="dashBoardSection">
+            <p>ceci est le dashboard</p>
+        </PageSection>
+    )
+}
+
+export default Connexion;
