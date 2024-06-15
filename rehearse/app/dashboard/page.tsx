da47bce816a1c8e29b4b4b7f457b67465a87986c
MajorDown <romain.fouillaron@gmx.fr>
2024-06-15 21:52:40 +0200

    ajouts de contexts

--- rehearse/app/dashboard/page.tsx
+++ rehearse/app/dashboard/page.tsx
@@ -1,10 +1,12 @@
 'use client'
 import PageSection from "@/components/PageSection";
+import { useUserContext } from "@/contexts/userContext";
 
 
 const Connexion = () => {
+    const { connectedUser } = useUserContext();
     return (
-        <PageSection title="Dashboard" id="dashBoardSection">
+        <PageSection title="Dashboard" id="dashBoardSection" needConnexion>
             <p>ceci est le dashboard</p>
         </PageSection>
     )
--- rehearse/app/options/page.tsx
+++ rehearse/app/options/page.tsx
@@ -1,10 +1,22 @@
+import Link from "next/link";
 import PageSection from "@/components/PageSection";
+
+// a prévoir dans les option : 
+// - modifier le mot de passe
+// - modifier le profile
+// - modifier les préférences éentuelles
+// - supprimer le compte
 
 const Options = () => {
     
     return (
         <PageSection title="Options" id="optionsSection" needConnexion>
-            <p>les options</p>
+            <h3>Que souhaitez-vous faire ?</h3>
+            <nav>
+                <ul>
+                    <li><Link href="/options/newPassword">Modifier votre mot de passe</Link></li>
+                </ul>
+            </nav>
         </PageSection>
     )
 }
